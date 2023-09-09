1d852634ed4dd434c037cdfdb6abff7e1c7eb771
Abhishek Anand <120253524+anand-1Abhishek@users.noreply.github.com>
2023-09-09 12:10:34 +0530

    setupopf smtp using nodemailer done

--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@ const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/serverConfig');
 
+const {sendBasicEmail} = require('./services/email-service')
+
 const setupAndStartServer = () =>{
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
 
     app.listen(3004,()=>{
-        console.log(`server started at port ${PORT}`)
+        console.log(`server started at port ${PORT}`);
+
+        sendBasicEmail(
+            'prabhushrianand@gmail.com',
+            'abhishekanand19472001@gmail.com',
+            'This is email testing',
+            'hy, how r you, I like You'
+        );
     });
 }
 
