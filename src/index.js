948e91cdd474edb71074d551db2dd0d96347af5e
Abhishek Anand <120253524+anand-1Abhishek@users.noreply.github.com>
2023-09-10 00:34:53 +0530

    Added notification cron

--- src/index.js
+++ src/index.js
@@ -3,24 +3,28 @@ const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/serverConfig');
 
-const {sendBasicEmail} = require('./services/email-service')
+//const {sendBasicEmail} = require('./services/email-service')
+const TicketController = require('./controllers/ticket-controller')
 
-const cron = require('node-cron')
+const jobs = require('./utils/job');
+
 
 const setupAndStartServer = () =>{
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
 
+    app.post('/api/v1/tickets',TicketController.create);
+
     app.listen(3004,()=>{
         console.log(`server started at port ${PORT}`);
-
-        sendBasicEmail(
-            'prabhushrianand@gmail.com',
-            'abhishekanand19472001@gmail.com',
-            'This is email testing',
-            'hy, how r you, I like You'
-        );
+        jobs();
+        // sendBasicEmail(
+        //     'prabhushrianand@gmail.com',
+        //     'abhishekanand19472001@gmail.com',
+        //     'This is email testing',
+        //     'hy, how r you, I like You'
+        // );
     });
 }
 
