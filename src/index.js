76eb006ce65679524bbe6d09a54ac90cf4bbce64
Abhishek Anand <120253524+anand-1Abhishek@users.noreply.github.com>
2023-09-13 10:21:25 +0530

    edited emailservice

--- src/index.js
+++ src/index.js
@@ -2,23 +2,31 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/serverConfig');
+const {createChannel,subscribeMessage} = require('./utils/messageQueue');
+const {REMINDER_BINDING_KEY} = require('./config/serverConfig');
 
 //const {sendBasicEmail} = require('./services/email-service')
 const TicketController = require('./controllers/ticket-controller')
+const EmailService = require('./services/email-service');
 
 const jobs = require('./utils/job');
 
 
-const setupAndStartServer = () =>{
+const setupAndStartServer = async () =>{
+
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
 
+    
     app.post('/api/v1/tickets',TicketController.create);
-
+    
+    const channel = await createChannel();
+    subscribeMessage(channel,EmailService.subscribeEvents,REMINDER_BINDING_KEY);
+    
     app.listen(3004,()=>{
         console.log(`server started at port ${PORT}`);
-        jobs();
+        //jobs();
         // sendBasicEmail(
         //     'prabhushrianand@gmail.com',
         //     'abhishekanand19472001@gmail.com',
